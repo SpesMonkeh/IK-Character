0f7f36e2153aa437b55b13f2e47b7721db00d49a
Christian Holm <chrholmchr@hotmail.com>
2023-01-05 04:01:01 +0100

    v0.4

--- Assets/Scripts/LegIKMover.cs
+++ Assets/Scripts/LegIKMover.cs
@@ -1,30 +1,76 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public sealed class LegIKMover : MonoBehaviour
 {
-	[SerializeField] float moveDistance;
+	[SerializeField] int isGrounded;
+	[SerializeField] int positionIndex;
+	[SerializeField] float liftDistance;
+	[SerializeField] float moveThreshold = .5f;
+	[SerializeField] float limbMoveSpeed = 1f;
+	[SerializeField] Vector3 halfwayPoint;
+	[SerializeField] Vector3 targetPoint;
 	[SerializeField] LayerMask groundLayer;
-	[SerializeField] Transform limbSolverTarget;
+	[SerializeField] Transform limbTarget;
+	[SerializeField] LegIKMover oppositeLimb;
+
+	RaycastHit2D groundCheckHit;
 
 	const float NORMAL_SCALE_MULTIPLIER = .1f;
 	const float GROUND_CHECK_RAY_DISTANCE = 5f;
+
+	public bool IsGrounded => isGrounded is 1;
+	public Vector3 Position => transform.position;
 	
 	public void CheckGround()
 	{
-		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, GROUND_CHECK_RAY_DISTANCE, groundLayer);
-		if (hit.collider == default) return;
+		groundCheckHit = Physics2D.Raycast(transform.position, Vector2.down, GROUND_CHECK_RAY_DISTANCE, groundLayer);
+		if (groundCheckHit.collider == default) return;
 		
-		Vector2 point = hit.point + hit.normal * NORMAL_SCALE_MULTIPLIER;
-		Debug.DrawLine(hit.point, point, Color.red);
+		Vector2 point = groundCheckHit.point + groundCheckHit.normal * NORMAL_SCALE_MULTIPLIER;
+		Debug.DrawLine(groundCheckHit.point, point, Color.red);
 		transform.position.Set(point.x, point.y, 0f);
 	}
 	
 	void Update()
 	{
 		CheckGround();
-
-		if (Vector2.Distance(limbSolverTarget.position, transform.position) <= moveDistance) return;
-		limbSolverTarget.position = transform.position;
+		DetermineMovement();
+		CheckIfGrounded();
 	}
+
+	void DetermineMovement()
+	{
+		Vector3 tfPos = transform.position;
+		Vector3 limbTargetPos = limbTarget.position;
+		
+		switch (positionIndex)
+		{
+			case 0 when oppositeLimb.IsGrounded && Vector3.Distance(limbTargetPos, tfPos) > moveThreshold:
+				targetPoint = tfPos;
+				halfwayPoint = (targetPoint + limbTargetPos) * .5f;
+				halfwayPoint.y += liftDistance;
+				positionIndex = 1;
+				break;
+			case 1:
+				UpdatePositionValues(ref limbTargetPos, halfwayPoint);
+				break;
+			case 2:
+				UpdatePositionValues(ref limbTargetPos, targetPoint);
+				break;
+		}
+
+		limbTarget.position = limbTargetPos;
+	}
+
+	void UpdatePositionValues(ref Vector3 limbTargetPos, Vector3 nextMovePoint)
+	{
+		const int max_pos_index = 2;
+		const float next_pos_dist = .1f;
+		int nextPosIndex = positionIndex >= max_pos_index ? 0 : positionIndex + 1;
+		limbTargetPos = Vector3.Lerp(limbTargetPos, nextMovePoint, limbMoveSpeed * Time.deltaTime);
+		if (Vector3.Distance(limbTargetPos, nextMovePoint) <= next_pos_dist)
+			positionIndex = nextPosIndex;
+	}
+	
+	void CheckIfGrounded() => isGrounded = positionIndex is 0 && groundCheckHit.transform != default ? 1 : 0;
 }
---
+++ Assets/Scripts/MovementHandler.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public sealed class MovementHandler : MonoBehaviour
+{
+	[SerializeField] float moveSpeed = 2f;
+	[SerializeField] float currentSpeed;
+	[SerializeField] float yOffset;
+	[SerializeField] float offsetTimer;
+	[SerializeField] float groundCheckDistance = 1.5f;
+	[SerializeField] float breathingSpeed = 1f;
+	[SerializeField] float breathingHeight = .5f;
+	[SerializeField] LayerMask groundLayer;
+	[SerializeField] Transform rayPoint;
+	[SerializeField] Rigidbody2D rigidBody;
+	[SerializeField] LegIKMover[] legTargets;
+
+	int isGrounded;
+	
+	const string HORIZONTAL = "Horizontal";
+
+	void Awake()
+	{
+		rigidBody = GetComponentInParent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		currentSpeed = moveSpeed * Input.GetAxis(HORIZONTAL);
+
+		Transform tf = transform;
+		Vector3 tfPos = tf.position;
+		tfPos.x += currentSpeed * Time.deltaTime;
+		tf.position = tfPos;
+		
+		CalculateGround();
+		Idle();
+	}
+
+	void FixedUpdate()
+	{
+		if (legTargets.Any(leg => leg.IsGrounded))
+		{
+			rigidBody.constraints |= RigidbodyConstraints2D.FreezePositionY;
+		}
+		else
+		{
+			rigidBody.constraints &= ~RigidbodyConstraints2D.FreezePositionY;
+			rigidBody.WakeUp();
+		}
+
+		isGrounded = rigidBody.constraints is RigidbodyConstraints2D.FreezePositionY ? 1 : 0;
+	}
+
+	void CalculateGround()
+	{
+		if (isGrounded is not 1) return;
+		
+		float offset = yOffset + offsetTimer;
+		RaycastHit2D hit = Physics2D.Raycast(rayPoint.position, Vector2.down, groundCheckDistance, groundLayer);
+		if (hit.collider == default) return;
+		
+		Vector3 point = legTargets.Aggregate(Vector3.zero, (current, legIK) => current + legIK.Position);
+		point.y = point.y / legTargets.Length + offset;
+
+		Transform tf = transform;
+		Vector3 tfPos = tf.position;
+		tfPos.y = point.y;
+		tf.position = tfPos;
+	}
+	
+	void Idle()
+	{
+		switch (Exhale())
+		{
+			case false when offsetTimer < breathingHeight:
+				offsetTimer += Time.deltaTime * (breathingSpeed * .1f);
+				break;
+			case true when offsetTimer > -breathingHeight:
+				offsetTimer -= Time.deltaTime * (breathingSpeed * .1f);
+				break;
+		}
+	}
+
+	bool Exhale()
+	{
+		if (offsetTimer > breathingHeight) return true;
+		return !(offsetTimer < -breathingHeight);
+	}
+}
